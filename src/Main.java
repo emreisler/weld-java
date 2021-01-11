01ef85ba81c4d71a3ccc0ac17059ba0624c5ffc1
emreisler <emre.isler.9@gmail.com>
2021-01-11 09:56:10 +0300

    Update Main.java
    Path is updated

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("uiV2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../path/to/uiV2.fxml"));
 
         stage.setTitle("Resistance Welding");
         stage.setScene(new Scene(root, 900, 600));
